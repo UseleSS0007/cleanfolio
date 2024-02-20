f7db6cc7a8be999975946cd4953af4f440810af4
SamiulNahiyan <agentsaif001@gmail.com>
2024-02-21 02:31:11 +0600

    Initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,62 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://saifahmed.github.io/portfolio',
+  title: 'SA.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Saif Ahmed',
+  role: 'Digital Marketer & Sales Representative',
+  description:
+    'Learning, exploring, gathering new information, and contributing some fresh thoughts are my main objectives. Work actively and establish a network of new relationships. Increasing my leadership potential and totally committing to my work.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'linkedin.com/in/saif-ahmed007/',
+    // github: 'https://github.com',
+  },
+}
+
+// const projects = [
+//   // projects can be added an removed
+//   // if there are no projects, Projects section won't show up
+//   {
+//     name: 'Project 1',
+//     description:
+//       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+//     stack: ['SASS', 'TypeScript', 'React'],
+//     sourceCode: 'https://github.com',
+//     livePreview: 'https://github.com',
+//   },
+//   {
+//     name: 'Project 2',
+//     description:
+//       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+//     stack: ['SASS', 'TypeScript', 'React'],
+//     sourceCode: 'https://github.com',
+//     livePreview: 'https://github.com',
+//   },
+//   {
+//     name: 'Project 3',
+//     description:
+//       'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+//     stack: ['SASS', 'TypeScript', 'React'],
+//     sourceCode: 'https://github.com',
+//     livePreview: 'https://github.com',
+//   },
+// ]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'Business Consultation',
+  'Sales Representative',
+  'Marketing',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'agentsaif001.com',
+}
+
+export { header, about, projects, skills, contact }
